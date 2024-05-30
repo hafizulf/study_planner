087c684fbcf8f443c670f2f60d373fa3b002072d
hafizulf <hafizulf68@gmail.com>
2024-05-30 22:36:20 +0700

    chore: add subject model

---
+++ src/models/subject.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class subject extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  subject.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'subject',
+      tableName: 'subjects',
+      underscored: true,
+    }
+  );
+  return subject;
+};
