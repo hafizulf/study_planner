4ff52c75938d69bb4c1f167f5e58512f6bd588fe
hafizulf <hafizulf68@gmail.com>
2024-05-30 22:27:07 +0700

    chore: add student model

---
+++ src/models/students.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Student.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      deletedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: 'student',
+      tableName: 'students',
+      underscored: true,
+    }
+  );
+  return Student;
+};
